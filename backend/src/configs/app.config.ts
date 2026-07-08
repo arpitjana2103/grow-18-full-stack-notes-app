332e1f89404b2453f0cf3f581f0b78284b4c5530
arpit jana <arpitjana2103@gmail.com>
2026-07-08 11:15:51 +0530

    update appconfig

--- backend/src/configs/app.config.ts
+++ backend/src/configs/app.config.ts
@@ -6,6 +6,22 @@ export const config = {
 
     SESSION_SECRET: getEnv("SESSION_SECRET"),
     SESSION_EXPIRES_IN: getEnv("SESSION_EXPIRES_IN"),
+
+    BACKEND_ORIGIN: getEnv("BACKEND_ORIGIN"),
+    get_BACKEND_GOOGLE_CALLBACK_URL: function (): string {
+        return getEnv("BACKEND_GOOGLE_CALLBACK_URL").replace(
+            "<backend_origin>",
+            this.BACKEND_ORIGIN,
+        );
+    },
+
+    FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN"),
+    get_FRONTEND_GOOGLE_CALLBACK_URL: function (): string {
+        return getEnv("FRONTEND_GOOGLE_CALLBACK_URL").replace(
+            "<frontend_origin>",
+            this.FRONTEND_ORIGIN,
+        );
+    },
 };
 
 export const runningOnProduction = function (): boolean {
